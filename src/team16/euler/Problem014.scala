a13e4c11fbff351fc9af67beabf25ae82b56678b
james <james@team16.co.uk>
2014-10-28 00:45:35 +0000

    Trying to use a table - turns out the Collatz sequence can get very large. Maybe the way to do it is an array of each size - the sequences are not actually that long.

--- src/team16/euler/Problem014.scala
+++ src/team16/euler/Problem014.scala
@@ -23,60 +23,24 @@ import scala.collection._
 object Problem014 extends App {
   val collatz = new Collatz
   //for(i <- 2 to 13) { println(collatz getLength2 i) }
-  println(collatz.getLength3(13))
-  println(collatz.getLength3(80))
+  println(collatz.getLength(13))
+  println(collatz.getLength(99996))
   
-  for(i <- 1 until 1000000) { collatz getLength3 i }
+  for(i <- 1 until 1000000) { collatz getLength i }
   println("Table built")
   println(collatz max)
 }
 
 class Collatz {
-  private val table = mutable.Map[Int, Int](1 -> 1); // Starting number -> length
+  private var table = new Array[Int](2); // Starting number -> length
   private val largest = 0
+  table(1) = 1
   
   def getLength(start: Int): Int = {
-    @scala.annotation.tailrec
-    def getLengthTC(start: Int, accumulator: Int): Int = {
-      val lookupLen = table get start
-      if(lookupLen != None) lookupLen get
-      else getLengthTC(next(start), accumulator+1)
-    }
-    val len = getLengthTC(start, 0)
-    table += start -> len
-    len
-  }
-  
-  def getLength2(start: Int): Int = {
-    @scala.annotation.tailrec
-    def getLengthTC(from: Int, accumulator: Int): Int = {
-      val successor = next(from)
-      val lookupLen = table get successor
-      if(lookupLen != None) {
-        val successorLen = lookupLen.get
-        table += (from -> (successorLen + 1))
-        accumulator + successorLen
-      }
-      else getLengthTC(successor, accumulator+1)
-    }
-    getLengthTC(start, 0)
-  }
-  
-  def getChainLengths(start: Int): List[Tuple2[Int,Int]] = {
-    val lookupLen = table get start
-    if(lookupLen != None) List((start, lookupLen get))
-    else {
-      val nextChainLengths = getChainLengths(next(start))
-      val chainLength = nextChainLengths.head._2
-      (start, chainLength+1) :: nextChainLengths
-    }
-  }
-  
-  def getLength3(start: Int): Int = {
     val chain = buildChain(start, List())
-    //println(chain)
+    println(chain)
     updateTable(chain)
-    //println(table)
+    println(table toString)
     val len = table(start)
     len
   }
@@ -84,20 +48,33 @@ class Collatz {
   // Build a chain until we hit a known value.
   @scala.annotation.tailrec
   private def buildChain(from: Int, accumulator: List[Int]): List[Int] = {
-    if(table contains from) from :: accumulator
+    if(table.length > from && table(from) != 0) from :: accumulator
     else buildChain(next(from), from :: accumulator)
   }
   
   // Update the table given a chain.
   @scala.annotation.tailrec
   private def updateTable(chain: List[Int]): Unit = {
-    val lastLen = table (chain.head)
+    val lastLen = table(chain head)
     val chainTail = chain tail
     
     if(!chainTail.isEmpty) {
-      table += chainTail.head -> (lastLen + 1)
+      addLookup(chainTail head, lastLen + 1)
       updateTable(chainTail)
     }
+  }
+  
+  private def addLookup(from: Int, length: Int): Unit = {
+    while(table.length <= from) {
+      growTable
+    }
+    table(from) = length
+  }
+  
+  private def growTable() = {
+    val oldTable = table
+    table = new Array[Int](table.length * 2)
+    oldTable.copyToArray(table)
   }
   
   def next(from: Int): Int = {
@@ -106,8 +83,9 @@ class Collatz {
   }
   
   def max() = {
-    //println(table)
-    table maxBy { case (x,y) => y }
+    println(table)
+    //table maxBy { case (x,y) => y }
+    table max
   }
   
 }
